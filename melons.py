96c016e9fbbeeec34c5391ec5b3589274a9f4380
Hackbright Student <no-reply@hackbrightacademy.com>
2016-10-07 13:06:51 -0700

    adding get_base methong to get_total

--- melons.py
+++ melons.py
@@ -1,5 +1,7 @@
 """This file should have our order classes in it."""
 
+#import random from choice or import random
+
 class AbstractMelonOrder(object):
     """Class to serve as base class and refractor"""
     def __init__(self, species, qty, order_type, tax):
@@ -15,10 +17,10 @@ class AbstractMelonOrder(object):
 
         base_price = 5
         total = (1 + self.tax) * self.qty * base_price
-        if species == "Christmas Melon":
+        if self.species == "Christmas Melon":
             return base_price * 1.5
-        if qty < 10:
-            return total + 3    
+        if self.qty < 10 and self.order_type == "international":
+            return total + 3
         return total
 
     def mark_shipped(self):
@@ -26,6 +28,27 @@ class AbstractMelonOrder(object):
 
         self.shipped = True
 
+    def get_base_price(self):
+
+        splurge_pricing = randint(5,9)
+        self.get_total
+        #
+
+
+
+class GovernmentMelonOrder(AbstractMelonOrder):
+    """A US government melon order"""
+
+    def __init__(self, species, qty, order_type, tax):
+        """Initialize melon order attributes."""
+        super(GovernmentMelonOrder, self).__init__(species, qty, "domestic", 0.00)
+        
+        self.passed_inspection = False
+
+    def mark_inspection(self, passed):
+        self.passed_inspection = passed
+
+ 
 class DomesticMelonOrder(AbstractMelonOrder):
     """A domestic (in the US) melon order."""
 
@@ -40,7 +63,7 @@ class InternationalMelonOrder(AbstractMelonOrder):
 
     def __init__(self, species, qty, country_code):
         """Initialize melon order attributes"""
-        super(DomesticMelonOrder, self).__init__(species, qty, "international", 0.08)
+        super(InternationalMelonOrder, self).__init__(species, qty, "international", 0.17)
     
         self.country_code = country_code
 
